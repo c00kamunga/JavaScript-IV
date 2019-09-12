3777950dfd7de399a4ce230b9f2695176ee8ebd9
c00kamunga <tsothalanti2@yahoo.com>
2019-09-11 17:46:43 -0700

    created a person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(civilian){
+        this.name = civilian.name;
+        this.age = civilian.age;
+        this.location = civilian.location;
+    }
+    speak(){
+        return `Hello my name is ${this.name}, I am from ${this.location}.`
+    }
+}

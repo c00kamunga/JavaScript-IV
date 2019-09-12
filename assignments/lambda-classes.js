7b82b31d0714ff392a526c2586429452cf569c26
c00kamunga <tsothalanti2@yahoo.com>
2019-09-11 17:53:43 -0700

    created an instructor class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,19 @@ class Person{
         return `Hello my name is ${this.name}, I am from ${this.location}.`
     }
 }
+
+
+
+class Instructor extends Person{
+    constructor(sensei){
+        super(sensei);
+        this.speceialy = sensei.specialty;
+        this.favlanguage = sensei.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`
+    }
+    grade(student, subject){
+        return `${student.name} recieves a perfect score on ${subject}.`
+    }
+}

9080286bb4733cfdd6b6303966fc72bedf13a8d6
c00kamunga <tsothalanti2@yahoo.com>
2019-09-11 18:04:51 -0700

    created a student class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -25,3 +25,22 @@ class Instructor extends Person{
         return `${student.name} recieves a perfect score on ${subject}.`
     }
 }
+
+
+class Student extends Person{
+    constructor(apprentice){
+        super(apprentice);
+        this.previousBackground = apprentice.previousBackground;
+        this.className = apprentice.className;
+        this.faSubjects = apprentice.favSubjects;
+    }
+    listsSubjects(){
+        console.log(this.favSubjects)
+    }
+    PRAssignment(subjects){
+        return `${this.name} has submitted a PR for ${subject}.`
+    }
+    sprintChallenge(subject){
+        return `${this.name} has begun sprint challenge on ${subject}.`
+    }
+}
